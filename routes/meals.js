301364c0a728e2e1512882d9147073b7af10cd51
mlinares <m.linares93@gmail.com>
2024-01-18 12:58:18 -0500

    make create route, controller, to save new meals to database

--- routes/meals.js
+++ routes/meals.js
@@ -3,7 +3,9 @@ import * as mealsCTRL from "../controllers/meals.js"
 
 const router = Router()
 
-// GET localhost:3000/flights
+// GET localhost:3000/meals/new
 router.get('/new', mealsCTRL.new)
+// POST localhost:3000/meals
+router.post('/', mealsCTRL.create)
 
 export { router }
